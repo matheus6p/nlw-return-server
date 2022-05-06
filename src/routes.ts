2d89e001f795ef1395a33d7fbe7ff24d8233adcd
matheus6p <mhpaes97@gmail.com>
2022-05-06 17:15:37 -0300

    remove trycatch

--- src/routes.ts
+++ src/routes.ts
@@ -1,29 +1,28 @@
-import { NodemailerMailAdapter } from "./adapters/nodemailer/nodemailer-mail-adapter";
+import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
 import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
 import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 import express from "express";
 
 export const routes = express.Router();
 
+
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
-  try {
-    const nodemailerMailAdapter = new NodemailerMailAdapter();
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
 
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-      prismaFeedbacksRepository,
-      nodemailerMailAdapter
-    );
+  const nodemailerMailAdapter = new NodemailerMailAdapter();
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
 
-    await submitFeedbackUseCase.execute({
-      type,
-      comment,
-      screenshot,
-    });
-    return res.status(201).send();
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send();
-  }
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository,
+    nodemailerMailAdapter
+  );
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
+  });
+
+
+  return res.status(201).send();
 });
